d54d13edb39e1255f25335ce1cb55592aea762e3
Marklenovich <daniyar01.m@gmail.com>
2021-06-03 01:24:10 +0600

    master

--- app/src/main/java/com/example/gamegeekcamp/MainActivity.java
+++ app/src/main/java/com/example/gamegeekcamp/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.gamegeekcamp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnPlay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnPlay = findViewById(R.id.btn_play);
 
+        btnPlay.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, GameScreen1.class);
+            startActivity(intent);
 
-
+        });
     }
 }
